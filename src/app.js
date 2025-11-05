e0ed9b1aecfdad5bcb7455ef746e7efdd3ac5668
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-11-05 18:45:39 -0300

    teste de rotas com rest client library

--- src/app.js
+++ src/app.js
@@ -7,14 +7,46 @@ dotenv.config();
 /* express instance */
 const app = express();
 
+/* mock */
+const users = [
+    {
+        id: 1,
+        name: 'Maria',
+        email: 'maria@email.com'
+    },
+    {
+        id: 2,
+        name: 'José',
+        email: 'jose@email.com'
+    },
+    {
+        id: 3,
+        name: 'Ana',
+        email: 'ana@email.com'
+    }
+]
+
 /* middlewares */
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // enable json read
 
 /* routes */
 app.get('/', (req, res) => {
     res.status(200).send("<h1>Server Online!</h1>");
 })
+
+/* show all users */
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+/* insert new data */
+app.post('/users', (req, res) => {
+    /* add new object */
+    users.push(req.body);
+    res.status(201).send('Seleção cadastrada com sucesso!');
+});
+                
 
 
 export default app;
